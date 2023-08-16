0b8ca440fb9ff27cee5a93c48ece3714ebfb50d9
saad <saadgfx97@gmail.com>
2023-08-16 11:26:49 +0500

    expenditure analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,40 @@
   - `npm run test-expenditure-analysis`
 */
 
+
+/**
+ * @typedef {Object} Transaction
+ *
+ * @property {number} id - The unique identifier for an item.
+ * @property {number} timestamp - The time at which the item was created. 
+ * This is represented as the number of milliseconds since 01 January, 1970 UTC.
+ * @property {number} price - The price of the item.
+ * @property {string} category - The category to which the item belongs.
+ * @property {string} itemName - The name of the item.
+ */
+
+/**
+ * @typedef {object} Expanse
+ * 
+ * @property {string} category
+ * @property {number} totalSpent
+ */
+
+/** 
+ * @param {Transaction[]} transactions
+ * @returns {Expanse[]}
+ */
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  
+  /** @type {Expanse[]} */
+  let totalExpanses = []
+  transactions.forEach(transaction => {
+    let expanseInList = totalExpanses.find(expanse => expanse.category == transaction.category)
+    if(expanseInList) expanseInList.totalSpent+=transaction.price
+    else totalExpanses.push({category: transaction.category, totalSpent: transaction.price})
+  })
+  
+  return totalExpanses
 }
 
 module.exports = calculateTotalSpentByCategory;
