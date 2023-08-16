c8706ed8c8870470e1127e3a0b4c2ea1e4f16c6f
saad <saadgfx97@gmail.com>
2023-08-16 10:55:28 +0500

    anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,30 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+/**
+ * 
+ * @param {string} str1 
+ * @param {string} str2 
+ * @returns {boolean}
+ */
 
+function isAnagram(str1, str2) {
+  // different lengths can't be anagram
+  if(str1.length !== str2.length) return false
+  
+  let str2Codes = str2.toLowerCase().split('').map(c => c.charCodeAt())
+  str1.toLowerCase().split('').map(c => c.charCodeAt()).forEach(code1 => {
+    let charInStr2 = str2Codes.find(code2 => code2 == code1)
+    if (!str2Codes || typeof charInStr2 == 'undefined') return false
+    
+    // remove the matching character from string 2 to handle duplications
+    str2Codes.splice(str2Codes.indexOf(charInStr2), 1)
+  })
+  
+  // if str2 has more characters
+  if(str2Codes.length) return false
+  
+  return true
 }
 
 module.exports = isAnagram;
